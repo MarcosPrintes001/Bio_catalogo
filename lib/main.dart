61e3b9ea17fb0c33bb39a265642e2e86f38a30ad
MarcosPrintes001 <mvprintes2@gmail.com>
2024-01-31 15:29:27 -0300

    removendo limitador de rotação de tela

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
 import 'package:bio_catalogo/pages/home.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 void main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await SystemChrome.setPreferredOrientations([
-  //   // DeviceOrientation.portraitUp,
-  //   // DeviceOrientation.portraitDown,
-  // ]);
   runApp(const MainApp());
 }
 
